603157d69dd7c7c7faeedc6dfd322f5ce73def93
張睿安 <a09817908381@outlook.com>
2025-12-20 07:03:27 +0000

    Add bookkeep_new Flutter project

---
+++ bookkeep_new/android/app/src/main/kotlin/com/example/bookkeep_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bookkeep_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bookkeep_new/lib/models/record.dart
@@ -0,0 +1,39 @@
+﻿class Record {
+  final int? id;
+  final String type; // 'income' 或 'expense'
+  final double amount;
+  final String category;
+  final String? note;
+  final DateTime date;
+
+  Record({
+    this.id,
+    required this.type,
+    required this.amount,
+    required this.category,
+    this.note,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'type': type,
+      'amount': amount,
+      'category': category,
+      'note': note,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory Record.fromMap(Map<String, dynamic> map) {
+    return Record(
+      id: map['id'],
+      type: map['type'],
+      amount: map['amount'],
+      category: map['category'],
+      note: map['note'],
+      date: DateTime.parse(map['date']),
+    );
+  }
+}
